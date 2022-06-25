399500b87dfad829448c2b91695cda2c6ff40dde
iammilankhati <iammilankhati@gmail.com>
2022-06-25 13:51:07 +0545

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About Us</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit, eveniet,
+        cum perferendis ab aliquam saepe doloremque nobis repudiandae aut totam
+        rem, numquam laborum commodi cumque error doloribus obcaecati autem
+        accusantium dolore excepturi? Tempore, exercitationem aliquam porro
+        reiciendis quibusdam dignissimos eligendi?
+      </p>
+    </section>
+  );
+};
+
+export default About;
