a7b65986ab07a53288e566acf22b47eee75cfa74
Milan Khati <91399021+iammilankhati@users.noreply.github.com>
2023-01-19 11:59:16 +0545

    this fixes #1
    i have the fixed the issues mentioned, login , register

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import CocktailList from "../components/CocktailList";
 import SearchForm from "../components/SearchForm";
 
 const Home = () => {
+  console.log('testing');
   return (
     <main>
       <SearchForm />
